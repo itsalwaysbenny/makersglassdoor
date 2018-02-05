7dae8b46d711c82379016f76c437c6030f802333
Pablo <paul.ladiv123@gmail.com>
2018-02-05 23:09:15 +0000

    add the reviews model and its respective seeds model for development

--- node-server/routes/users.js
+++ node-server/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
@@ -8,7 +9,7 @@ router.get('/', function(req, res, next) {
     // And insert something like this instead:
     res.json([{
         id: 1,
-        username: "samsepi0l"
+        username: "J0hn213"
     }, {
         id: 2,
         username: "D0loresH4ze"
