7b26e5ae8ce272788d4f44c3e0ac5a03589fb62d
Pablo <paul.ladiv123@gmail.com>
2018-02-05 22:42:27 +0000

    create seeds file for users model

---
+++ node-server/db/seeds/development/users_seed.js
@@ -0,0 +1,21 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert({
+          email: 'johnsmith@email.com',
+          name: 'John Smith',
+          password: 'totallySecret',
+          current_position: "javascript junior dev"
+      });
+    }).then(function () {
+          // Inserts seed entries
+          return knex('users').insert({
+              email: 'alicewonder@email.com',
+              name: 'Alice Wonder',
+              password: 'pAssw0rd',
+              current_position: "Ruby junior dev"
+          });
+      });
+};
