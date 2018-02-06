e14cde86ae4a0842cc2f7e9ca58ee616b33f954f
Pablo <paul.ladiv123@gmail.com>
2018-02-06 11:59:07 +0000

    add knex.js database connexion with users table in database

--- node-server/routes/users.js
+++ node-server/routes/users.js
@@ -1,19 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var database = require('../models/database');
 
+console.log("DATABASE*******", database);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
 
+  var testKnex =  database.select('name', 'email', 'current_position').from('users')
+console.log("1&&&&&&&&&",testKnex);
+
+    database('users').select('name').then(function(response){
+        //do something here
+        // console.log(projectNames);
+       return res.json(response);
+    });
+
+
     // And insert something like this instead:
-    res.json([{
-        id: 1,
-        username: "J0hn213"
-    }, {
-        id: 2,
-        username: "D0loresH4ze"
-    }]);
+    // res.json([{
+    //     id: 1,
+    //     username: "J0hn213"
+    // }, {
+    //     id: 2,
+    //     username: "D0loresH4ze"
+    // }]);
 });
 
 module.exports = router;
