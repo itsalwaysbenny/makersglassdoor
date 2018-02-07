b93e3f75203176b65f59e62564276064695322b1
Pablo <paul.ladiv123@gmail.com>
2018-02-07 12:47:46 +0000

    add knex syntax to retrieve data from the database

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 
 class App extends Component {
 
+    state = {users: []}
+
+    componentDidMount {
+        fetch('/users')
+    .then(res => res.json())
+    .then(users => this.setState({users}));
+}
+
 
   render() {
     return (
--- node-server/routes/users.js
+++ node-server/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../knex');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+    knex('users').select('name').then(function(response){
+        return res.json(response);
+    });
 });
 
 module.exports = router;
