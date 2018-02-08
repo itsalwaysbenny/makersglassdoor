622d7f2082d0b77cdb5afd5e3aa855786d3cf1e2
Pablo <paul.ladiv123@gmail.com>
2018-02-08 23:06:37 +0000

    add data to be used with the seed files

---
+++ node-server/data/reviews.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        text: 'very challenging but worth it',
+        author_id: 'johnsmith@email.com',
+        company_id: 'Awesome Tech ltd.'
+    },
+    {
+        text: 'Totally awesome',
+        user: 'mary@email.com',
+        company_id: 'WhatEverHappens ltd'
+
+    },
+
+    {
+        text: 'You have to work real hard but totally worth every second',
+        user: 'Francis@email.com',
+        company_id: 'WhatEverHappens ltd'
+    },
+
+    {
+        text: 'very corporate environment',
+        user: 'Deborah@email.com',
+        company_id: 'This is it'
+    },
+
+    {
+        text: 'wow',
+        user: 'herbert@email.com',
+        company_id: 'This is it'
+    }
+
+]
