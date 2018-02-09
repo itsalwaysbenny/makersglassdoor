670a67edb00eab1831e26a7f6bbaec590220ed5d
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2018-02-09 12:27:50 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-
-    state = {users: []}
-
-    componentDidMount() {
-        fetch('/users')
-            .then(res=>res.json())
-    .then(users => this.setState({users}));
-    }
-
-  render() {
-    return (
-      <div className="App">
-          <h1> Makers GlassDoor App </h1>
-      </div>
-    );
-  }
-}
+ class App extends Component {
+   render() {
+     return (
+       <div className="App">
+         <header className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           <h1 className="App-title">Welcome to React</h1>
+         </header>
+         <p className="App-intro">
+           To get started, edit <code>src/App.js</code> and save to reload.
+         </p>
+       </div>
+     );
+   }
+ }
 
 export default App;
