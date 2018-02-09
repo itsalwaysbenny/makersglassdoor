4681b1cffb36162ff01486d4dc18a258b54d4104
Pablo123GitHub <paul.ladiv123@gmail.com>
2018-02-09 15:08:01 +0000

    Update users_reviews_seed.js

--- node-server/db/seeds/development/users_reviews_seed.js
+++ node-server/db/seeds/development/users_reviews_seed.js
@@ -29,26 +29,6 @@ exports.seed = function(knex, Promise) {
 });
 };
 
-// .then(()=>{
-//
-//     let companiesPromises = [];
-//     companiesData.forEach((company)=>{
-//         console.log("Do I reach here ");
-//         let companyName = company.name;
-//     companiesPromises.push(createReviewCompany(knex,company, companyName ));
-//     })
-//     });
-
-
-//     let companiesPromises = [];
-//     reviewsData.forEach((company)=>{
-//         let companyName = company.name;
-//     companiesPromises.push(createCompany(knex,company,companyName));
-// });
-
-
-
-
 const createReview = (knex, review, userEmail) => {
 
     return knex('users').where('email', userEmail).first()
@@ -60,44 +40,4 @@ const createReview = (knex, review, userEmail) => {
     });
     });
 };
-
-
-// const createReviewCompany = (knex, review, companyName) => {
-//     return knex('companies').where('name', companyName).first()
-//         .then((companyRecord)=>{
-//         return knex('reviews').insert({
-//             company_id: companyRecord.id
-//         });
-// });
-// };
-
-
-
-// const createProduct = (knex, product, merchant) => {
-//     return knex(‘merchants’).where(‘name’, merchant).first()
-//         .then((merchantRecord) => {
-//         return knex(‘products’).insert({
-//         name: product.name,
-//         price: product.price,
-//         merchant_id: merchantRecord.id
-//     });
-// });
-// };
-
-//
-// (userRecord)=>{
-//     return knex('reviews').insert({
-//         text: review.text,
-//         author_id: userRecord.id
-//     });
-// }
-// const createCompany = (knex, review,companyName) => {
-//     return knex('companies').where('name', companyName).first()
-//         .then((companyRecord)=>{
-//         return knex('reviews').insert({
-//             company_id: companyRecord.id
-//         });
-// });
-// };
-
 
