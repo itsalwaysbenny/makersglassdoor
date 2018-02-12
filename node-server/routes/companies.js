361b49051d2dc1ad27e26310b4b24ec13eabb107
Pablo <paul.ladiv123@gmail.com>
2018-02-12 16:58:02 +0000

    slight changes to the companies.js file : more static exampes

--- node-server/routes/companies.js
+++ node-server/routes/companies.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
+function showCompanies(){
+    return knex.select().from('companies')
+        .then(function(row){
+            console.log('querying Companies', row);
+        });
 
-
+}
 
 /* GET companies page. */
 router.get('/', function(req, res, next) {
+
+    // showCompanies();
 
     res.json([{
         id: 1,
@@ -13,7 +21,18 @@ router.get('/', function(req, res, next) {
     }, {
         id: 2,
         name: "acme2"
-    }]);
+    },
+        {
+            id: 3,
+            name: "facebook"
+        },
+
+    {
+        id: 4,
+            name: "twitter"
+    }])
+
+    ;
 });
 
 module.exports = router;
